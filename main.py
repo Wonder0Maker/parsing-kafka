294334684876f764d4a6a00934423a176558fadb
Alina Mandryk <mandryk@nixsolutions.com>
2021-06-25 15:54:09 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from producer import producer, parse_site, parse_items
+
+if __name__ == '__main__':
+    url = "https://www.allrecipes.com/recipes/859/desserts/cookies/sugar-cookies/"
+
+    producer = producer()
+    items, count_of_items = parse_site(producer, url)
+
+    for i in range(0, count_of_items):
+        parse_items(producer, items, i)
---
+++ producer.py
@@ -0,0 +1,53 @@
+from kafka import KafkaProducer
+import json
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from time import sleep
+
+
+def producer():
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                             value_serializer=lambda v:
+                             json.dumps(v, default=str).encode('utf-8'))
+
+    return producer
+
+
+def parse_site(producer, url):
+    response = urlopen(url)
+    html = response.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+
+    text_links = soup.findAll('a', class_='category-page-item-image-text')
+
+    list_of_links = []
+
+    for link in text_links:
+        address = link["href"]
+        text = link.text
+        text = text[1:]
+        data = {text: address}
+        list_of_links.append(address)
+        producer.send('links', value=data)
+        sleep(1)
+
+    print(list_of_links)
+
+    count_of_links = len(list_of_links)
+
+    return list_of_links, count_of_links
+
+
+def parse_items(producer, list_of_links, number_link):
+    url = list_of_links[number_link]
+    response = urlopen(url)
+    html = response.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    links = soup.findAll('li', class_='ingredients-item')
+
+    for link in links:
+        text = link.text
+        text = text[4:-3]
+        print(text)
+        producer.send('meal', value=text)
+        sleep(1)
